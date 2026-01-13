ed4fb7fd34a1278c3417e35f40510dbef209ee8e
Kyara Schmitz <schmitzk@shernet.sheridancollege.ca>
2026-01-13 11:06:20 -0500

    Made the teleporting fuction properly

--- Assets/Scripts/ThreeSecondTimer.cs
+++ Assets/Scripts/ThreeSecondTimer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ThreeSecondTimer : MonoBehaviour
 {
+    float t;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,15 @@ public class ThreeSecondTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newPos = transform.position;
+
+        t += Time.deltaTime;
+        if (t > 3)
+        {
+            t = 0;
+            newPos.x = Random.Range(-10, 10);
+            newPos.y = Random.Range(-5, 5);
+            transform.position = newPos;
+        }
     }
 }
